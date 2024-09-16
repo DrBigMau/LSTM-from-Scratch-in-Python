6ce440e1539088e44836e3b5329374f17e575d1f
DrBigMau <165067250+DrBigMau@users.noreply.github.com>
2024-09-16 18:20:24 +0200

    Update LSTM.py
    Issue 1 fixed, sigmoid class. Now:
    class Sigmoid:
            
        def forward(self, M):
            
            sigm        = np.clip(1/(1 + np.exp(-M)), 1e-7, 1 - 1e-7)
            self.output = sigm #needed for back prop
            self.inputs = M 
                
        def backward(self, dvalues):
            
            sigm         = self.output
            deriv        = np.multiply(sigm, (1 - sigm))
            self.dinputs = np.multiply(deriv, dvalues)

--- 5 package/LSTM.py
+++ 5 package/LSTM.py
@@ -459,12 +459,12 @@ class Sigmoid:
     def forward(self, M):
         
         sigm        = np.clip(1/(1 + np.exp(-M)), 1e-7, 1 - 1e-7)
-        self.output = sigm
-        self.inputs = sigm #needed for back prop
+        self.output = sigm #needed for back prop
+        self.inputs = M
             
     def backward(self, dvalues):
         
-        sigm         = self.inputs
+        sigm         = self.output
         deriv        = np.multiply(sigm, (1 - sigm))
         self.dinputs = np.multiply(deriv, dvalues)
         
